9b750163e597530e3f6b5bffe36aacc7414b124e
KirillProgramSite <kproskuri@gmail.com>
2025-10-16 18:24:53 +0300

    update link footer 0.1.0

--- rl/src/components/footer/Footer.tsx
+++ rl/src/components/footer/Footer.tsx
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <Flex mt={100} mb={50} justify="space-between">
             <Link colorPalette="teal" variant="underline" target="_blank" href="https://github.com/KirillProgramSite/readlater">GitHub</Link>           
-            <Link colorPalette="teal" variant="underline" target="_blank" href="https://github.com/KirillProgramSite/readlater/releases/tag/0.0.1">0.0.1</Link>
+            <Link colorPalette="teal" variant="underline" target="_blank" href="https://github.com/KirillProgramSite/readlater/releases/tag/0.1.0">0.1.0</Link>
         </Flex>
     )
 }
